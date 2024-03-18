59f840f16e49698be2c9527d699451931fd229f1
jo <20772026@qq.com>
2024-03-18 21:47:45 +0800

    数据库修复

--- login/models.py
+++ login/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.base_user import AbstractBaseUser,BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def crate_user(self,email,username,password):
+    def create_user(self,email,username,password):
         user = self.model(
             email = self.normalize_email(email),
             username = username,
@@ -13,8 +13,8 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-    def crate_superuser(self,email,username,password):
-        user = self.crate_user(email,
+    def create_superuser(self,email,username,password):
+        user = self.create_user(email,
                                username,
                                password)
         user.is_admin = True
